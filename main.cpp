6ba767ff45cfda20966e50fdf4d9817595af0ac3
TinNguyen <ductin16111@gmail.com>
2022-06-08 10:57:28 +0300

    create library file mylib.hh and main file main.cpp

---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include "mylib.hh"
+
+int main(void){
+    cout << "hello";
+}
---
+++ mylib.hh
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <string.h>
+#include <conio.h>
+#include <cstdio>
+#include <ctime>
+#include <stdio.h>
+#include <Windows.h>
+#include <cstdlib>
+#include <fstream>
+using namespace std;
