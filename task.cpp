e49d376dcc05a17c881074c793996b64cf7f05fd
TinNguyen <ductin16111@gmail.com>
2022-06-14 10:13:11 +0300

    add switch-case in loop() and codes for option 3

--- task.cpp
+++ task.cpp
@@ -2,12 +2,35 @@
 
 void print_home_screen(){
     system("cls");
-    cout << endl << "============ Welcome to authentication system ============" << endl << endl;
+    cout << endl << "========== Welcome to authentication system ==========" << endl << endl;
     cout << "1. Signup" << endl;
     cout << "2. Login" << endl;
     cout << "3. Exit" << endl;
 }
 
 void loop(User *user, int count){
-    cout << "hello";
+    print_home_screen();
+    cout << "Select an operation: ";
+    char ch; cin >> ch;
+    while(int(ch) < 49 || int(ch)>51){
+        cout << "Invalid input!!! Please try again." << endl;
+        cout << "Select an operation: ";cin >> ch;
+    }
+    int choice = int(ch) - 48;
+    switch (choice)
+    {
+        case 1:{
+
+            break;
+        }
+        case 2:{
+            break;
+        }
+        case 3:{
+            delete user;
+            cout << "\t\t\tGood Bye =))" << endl << endl;
+            exit(0);
+            break;
+        }
+    }
 }
